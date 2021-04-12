0f11d219ee47be0864ea12e6d5b76343ae5208d6
Andy McGrath <andy.mcgrath@boomin.com>
2021-04-12 17:40:29 +0100

    Set check interval at root of config file and set state of urlcheck on initial request

--- main.go
+++ main.go
@@ -7,12 +7,12 @@ import (
 
 func main() {
 
-	contacts, urls := config.GetConfig()
+	interval, contacts, urls := config.GetConfig()
 
 	for {
 		for _, url := range urls {
 			go url.WebChecker(contacts)
 		}
-		time.Sleep(300 * time.Second)
+		time.Sleep(time.Duration(interval) * time.Second)
 	}
 }
